1ce7072150e7c389ff32d4351d92aa19dfb367bf
Shay <yourlocalqueerincubus@gmail.com>
2026-03-24 21:28:53 -0400

    refactor index.js to use basePath for static files

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const basePath = path.join(__dirname, 'assignment-2');
+
+app.use(express.static(basePath));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(basePath, 'index.html'));
+});
+
+app.get('/api', (req, res) => {
+    const data = fs.readFileSync(path.join(basePath, 'db.json'));
+    res.json(JSON.parse(data));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(basePath, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+// Live URL: https://assignment-2-u9ke.onrender.com
